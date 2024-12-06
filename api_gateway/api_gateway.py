8157588de6b1b651ba116c18abf56bd27d17d666
Reidyensam <reidyensam@gmail.com>
2024-12-06 17:20:16 -0400

    Se ha agregado contacto y conecciones con la base de datos

--- api_gateway/api_gateway.py
+++ api_gateway/api_gateway.py
@@ -1,16 +1,38 @@
-import sys
-import os
-from flask import Flask
-from routes import init_routes
+from flask import Flask, render_template, redirect, request
+import requests
 
-# Agregar la ruta raíz del proyecto al sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+app = Flask(__name__)
 
-# Configurar Flask para usar la carpeta de plantillas y estáticos correcta
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.config.from_object('config.Config')
+def init_routes(app):
+    @app.route('/')
+    def index():
+        return render_template('inicio.html')
 
-# Inicializar las rutas
+    @app.route('/contacto')
+    def contacto():
+        return render_template('contacto.html')
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    @app.route('/registro')
+    def registro():
+        return render_template('registro.html')
+
+    @app.route('/crear_evento')
+    def crear_evento():
+        return render_template('crear_evento.html')
+
+    @app.route('/usuarios', methods=['POST'])
+    def create_user():
+        data = request.form.to_dict()
+        response = requests.post('http://localhost:5001/usuarios', json=data)
+        if response.status_code == 201:
+            return redirect('/crear_evento')
+        else:
+            return 'Error: Usuario no creado', response.status_code
+
 init_routes(app)
 
 if __name__ == "__main__":
--- api_gateway/routes.py
+++ api_gateway/routes.py
@@ -5,8 +5,20 @@ def init_routes(app):
     def index():
         return render_template('inicio.html')
 
+    @app.route('/contacto')
+    def contacto():
+        return render_template('contacto.html')
+
     @app.route('/registro')
     def registro():
         return render_template('registro.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+    
+    @app.route('/crear_evento')
+    def crear_evento():
+        return render_template('crear_evento.html')
     
     # Otras rutas pueden ser agregadas aquí...
